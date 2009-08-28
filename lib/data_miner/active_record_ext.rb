e62714e49f878c3ddb026ca3aa150d55dcf7dcac
Seamus Abshere <seamus@abshere.net>
2009-08-28 11:50:58 -0400

    save memory if NO_DATA_MINER is set

--- lib/data_miner/active_record_ext.rb
+++ lib/data_miner/active_record_ext.rb
@@ -6,9 +6,17 @@ module DataMiner
 
     module ClassMethods
       def mine_data(options = {}, &block)
-        class_eval { cattr_accessor :data_mine }
-        self.data_mine = Configuration.new(self)
-        yield data_mine
+        if defined?(:NO_DATA_MINER) and ::NO_DATA_MINER == true
+          class_eval do
+            def data_mine
+              raise "NO_DATA_MINER is set to true, so data_mine is not available"
+            end
+          end
+        else
+          class_eval { cattr_accessor :data_mine }
+          self.data_mine = Configuration.new(self)
+          yield data_mine
+        end
       end
     end
   end
