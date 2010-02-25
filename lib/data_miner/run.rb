5e112393f73a5a2c1507a96475eb3aca97a67738
Seamus Abshere <seamus@abshere.net>
2010-02-25 15:36:54 -0500

    Fix Rails bug "X is not missing constant Y"
    http://enterpriserails.chak.org/forum/topics/rails-bug-x-is-not-missing

--- lib/data_miner/run.rb
+++ lib/data_miner/run.rb
@@ -2,6 +2,6 @@ module DataMiner
   class Run < ActiveRecord::Base
     set_table_name 'data_miner_runs'
     default_scope :order => 'id ASC'
-    belongs_to :target
+    belongs_to :target, :class_name => '::DataMiner::Target'
   end
 end
--- lib/data_miner/target.rb
+++ lib/data_miner/target.rb
@@ -2,6 +2,6 @@ module DataMiner
   class Target < ActiveRecord::Base
     set_table_name 'data_miner_targets'
     set_primary_key :name
-    has_many :runs, :foreign_key => 'data_miner_target_id'
+    has_many :runs, :class_name => '::DataMiner::Run', :foreign_key => 'data_miner_target_id'
   end
 end
