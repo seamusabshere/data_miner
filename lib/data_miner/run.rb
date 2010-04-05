d64cb1d86a6be61b203cbc96d23ecc2f2d3daf5c
Seamus Abshere <seamus@abshere.net>
2010-04-05 13:25:56 -0400

    Don't use default_scope, which breaks in Rails 3.

--- lib/data_miner/run.rb
+++ lib/data_miner/run.rb
@@ -1,7 +1,6 @@
 module DataMiner
   class Run < ActiveRecord::Base
     set_table_name 'data_miner_runs'
-    default_scope :order => 'id ASC'
     
     def resource
       resource_name.constantize
