edcfa15f1923b7374d56d79485dfc19a057d79f2
Seamus Abshere <seamus@abshere.net>
2010-05-21 11:40:00 -0400

    when tapping, pull indexes first.

--- lib/data_miner/tap.rb
+++ lib/data_miner/tap.rb
@@ -104,7 +104,7 @@ module DataMiner
     
     # taps pull mysql://root:password@localhost/taps_test http://foo:bar@data.brighterplanet.com:5000 --tables aircraft
     def taps_pull_cmd
-      "taps pull #{adapter}://#{db_locator} #{source} --tables #{source_table_name}"
+      "taps pull #{adapter}://#{db_locator} #{source} --indexes-first --tables #{source_table_name}"
     end
     
     # 2.3.5 mysql
