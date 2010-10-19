4a5b246eb498e0da96e8d54200372faa86056d30
Derek Kastner <dkastner@gmail.com>
2010-10-19 11:27:42 -0400

    Removed ruby-debug

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
-require 'ruby-debug'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
