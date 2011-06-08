1939f8218374df3a1b8ff04f3632b2616cdf050d
Seamus Abshere <seamus@abshere.net>
2011-06-08 17:44:02 -0500

    add run_data_miner_on_parent_associations!
    also flatten module

--- lib/data_miner/active_record_extensions.rb
+++ lib/data_miner/active_record_extensions.rb
@@ -3,25 +3,35 @@ require 'blockenspiel'
 
 class DataMiner
   module ActiveRecordExtensions
+    def data_miner_config
+      @data_miner_config ||= ::DataMiner::Config.new self
+    end
+    
+    def data_miner_config=(config)
+      @data_miner_config = config
+    end
+    
+    def data_miner_runs
+      ::DataMiner::Run.scoped :conditions => { :resource_name => name }
+    end
+
+    def run_data_miner!(options = {})
+      data_miner_config.run options
+    end
+    
+    def run_data_miner_on_parent_associations!
+      reflect_on_all_associations(:belongs_to).each do |assoc|
+        next if assoc.options[:polymorphic]
+        assoc.klass.run_data_miner!
+      end
+    end
+    
     def data_miner(options = {}, &blk)
       ::DataMiner.instance.start_logging
 
       ::DataMiner.instance.resource_names.push self.name unless ::DataMiner.instance.resource_names.include? self.name
 
-      # this is class_eval'ed here so that each ActiveRecord descendant has its own copy, or none at all
-      class_eval do
-        cattr_accessor :data_miner_config
-        def self.data_miner_runs
-          ::DataMiner::Run.scoped :conditions => { :resource_name => name }
-        end
-        def self.run_data_miner!(options = {})
-          data_miner_config.run options
-        end
-      end
-
-      if options[:append]
-        self.data_miner_config ||= ::DataMiner::Config.new self
-      else
+      unless options[:append]
         self.data_miner_config = ::DataMiner::Config.new self
       end
 
