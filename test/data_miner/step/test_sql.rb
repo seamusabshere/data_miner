f62ec2a394378e5808ebf31fa8ac453af2493881
Seamus Abshere <seamus@abshere.net>
2013-07-24 21:06:59 -0500

    fix sql test

--- test/data_miner/step/test_sql.rb
+++ test/data_miner/step/test_sql.rb
@@ -2,37 +2,33 @@
 require 'helper'
 init_database
 
-class BreedBlue < ActiveRecord::Base
-  self.table_name = 'breeds'
-  self.primary_key = 'name'
+class StateBlue < ActiveRecord::Base
+  self.table_name = 'states'
+  self.primary_key = 'postal_abbreviation'
   data_miner do
-    sql "Brighter Planet's list of breeds (as a URL)", 'http://data.brighterplanet.com/breeds.sql'
+    sql "Brighter Planet's list of states (as a URL)", 'http://data.brighterplanet.com/states.sql'
   end
 end
 
-class BreedRed < ActiveRecord::Base
-  self.table_name = 'breeds'
-  self.primary_key = 'name'
+class StateRed < ActiveRecord::Base
+  self.table_name = 'states'
+  self.primary_key = 'postal_abbreviation'
   data_miner do
-    sql "Brighter Planet's list of breeds (as a URL)", 'http://data.brighterplanet.com/breeds.sql'
-    sql "Mess up weights", %{UPDATE breeds SET weight = 999}
+    sql "Brighter Planet's list of states (as a URL)", 'http://data.brighterplanet.com/states.sql'
+    sql "Mess up weights", %{UPDATE states SET name = 'Foobar'}
   end
 end
 
 describe DataMiner::Step::Sql do
   before do
-    BreedBlue.delete_all rescue nil
+    StateBlue.delete_all rescue nil
   end
   it "can be provided as a URL" do
-    BreedBlue.run_data_miner!
-    BreedBlue.where(:name => 'Affenpinscher').count.must_equal 1
-    BreedBlue.where(:name => 'Württemberger').count.must_equal 1
-    BreedBlue.find('Afghan Hound').weight.must_be_close_to 24.9476
+    StateBlue.run_data_miner!
+    StateBlue.where(:name => 'Wisconsin').count.must_equal 1
   end
   it "can be provided as a string" do
-    BreedRed.run_data_miner!
-    BreedRed.where(:name => 'Affenpinscher').count.must_equal 1
-    BreedRed.where(:name => 'Württemberger').count.must_equal 1
-    BreedRed.find('Afghan Hound').weight.must_be_close_to 999
+    StateRed.run_data_miner!
+    StateRed.find('NJ').name.must_equal 'Foobar'
   end
 end
